5b1b7d4400dec96bd2f42555499f7b772da2649d
Savet Hong <savet3@gmail.com>
2017-03-19 17:09:07 -0700

    Simplify Age Group Categories.

--- CA2015.R
+++ CA2015.R
@@ -11,7 +11,8 @@
 library(acs)
 library(reshape2)
 library(ggplot2)
-
+library(dplyr)
+library(plyr)
 
 # Get the Data
 acs.lookup(2010,keyword = "age")
@@ -42,15 +43,26 @@ ca.m.df <- data.frame(estimate(ca.pop.m))
 names(ca.f.df) <- gsub("Sex.by.Age..Female..","",names(ca.f.df))
 names(ca.m.df) <- gsub("Sex.by.Age..Male..","",names(ca.m.df))
 ca.pop.df <- rbind(ca.m.df,ca.f.df)
-ca.pop.df[1,] <- -1*ca.pop.df[1,]
 ca.pop.df$Sex <- c("Male","Female")
 
-ca.long <- melt(ca.pop.df, id.vars = "Sex", variable.name = "Age", value.name = "Population")
+ca.long <- melt(ca.pop.df, id.vars = "Sex", variable.name = "Age Group", value.name = "Population")
+ca.long <- ca.long %>%
+  mutate(`Age Group` =revalue(`Age Group`,c("15.to.17.years" = "15.to.19.years",
+                                            "18.and.19.years" = "15.to.19.years",
+                                            "20.years"="20.to.24.years",
+                                            "21.years"="20.to.24.years",
+                                            "22.to.24.years" ="20.to.24.years", 
+                                            "60.and.61.years"="60.to.64.years", 
+                                            "62.to.64.years"="60.to.64.years",
+                                            "65.and.66.years"="65.to.69.years",
+                                            "67.to.69.years"="65.to.69.years"))) %>%
+  group_by(Sex, `Age Group`) %>%
+  summarise(Population =sum(Population)) %>%
+  mutate(Population = ifelse(Sex=="Male", -1*Population,Population))
 
 
-
-ggplot(ca.long, aes(x = Age, y = Population, fill= Sex)) +
-  geom_bar(subset=.(Sex="Female"), stat = "identity") +
-  geom_bar(subset =.(Sex="Male"), stat = "identity") +
+ggplot(ca.long, aes(x = `Age Group`, y = Population, fill= Sex)) +
+  geom_bar(stat = "identity") +
   coord_flip()
-
+## Simplify Age Group into 5yrs interval
+age <- c(paste0(seq(0,80,5),"-",seq(4,84,5)),"85+")
