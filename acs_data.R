18f989c83060e720c9340b27fe266694ccc9c0a3
Savet <savethong@Savets-MacBook-Air.local>
2017-05-06 10:04:38 -0700

    Debugged sacs function to generate dataframe for multiple years of ACS data.

--- acs_data.R
+++ acs_data.R
@@ -5,46 +5,51 @@
 # Extract ACS DATA
 ######################
 
-# State only
-
-sacs <- function(state, year){
-  st <- gsub(" ","",toupper(state))
-  sex.age.m <- acs.lookup(table.number = "B01001", endyear = year, keyword = "Male")[-1]
-  sex.age.f <- acs.lookup(table.number = "B01001", endyear = year, keyword = "Female")[-1]
-  ca.pop.m <- acs.fetch(geography = geo.make(state = "CA"), endyear = year, variable = sex.age.m, col.names = "pretty" )
-  ca.pop.f <- acs.fetch(geography = geo.make(state = "CA"), endyear = year, variable = sex.age.f, col.names = "pretty" )
-  
-  ca.f.df <- data.frame(estimate(ca.pop.f))
-  ca.m.df <- data.frame(estimate(ca.pop.m))
-  names(ca.f.df) <- gsub("Sex.by.Age..Female..","",names(ca.f.df))
-  names(ca.m.df) <- gsub("Sex.by.Age..Male..","",names(ca.m.df))
-  ca.pop.df <- rbind(ca.m.df,ca.f.df)
-  ca.pop.df$Sex <- c("Male","Female")
-  
-  ca.long <- melt(ca.pop.df, id.vars = c("Year","Sex"), variable.name = "Age", value.name = "Population")
-  ca.long <- ca.long %>%
-    mutate(Age = revalue(Age, c("15.to.17.years" = "15.to.19.years",
-                                  "18.and.19.years" = "15.to.19.years",
-                                  "20.years"="20.to.24.years",
-                                  "21.years"="20.to.24.years",
-                                  "22.to.24.years" ="20.to.24.years", 
-                                  "60.and.61.years"="60.to.64.years", 
-                                  "62.to.64.years"="60.to.64.years",
-                                  "65.and.66.years"="65.to.69.years",
-                                  "67.to.69.years"="65.to.69.years"))) %>%
-    group_by(Year,Sex, Age) %>%
-    summarise(Population =sum(Population))
-  
-  ca.long$Age <- gsub(".years","",ca.long$Age)
-  ca.long$Age <- gsub(".to.","-",ca.long$Age)
-  ca.long$Age[ca.long$Age=="Under.5"] <- "0-4"
-  ca.long$Age[ca.long$Age=="85.and.over"] <- "85+"
-  
-  return(ca.long)
+# CA State only
+acs <- function(year){
+    sacs_single <- function(year_sing){
+      sex.age.m <- acs.lookup(table.number = "B01001", endyear = year_sing, keyword = "Male")[-1]
+      sex.age.f <- acs.lookup(table.number = "B01001", endyear = year_sing, keyword = "Female")[-1]
+      ca.pop.m <- acs.fetch(geography = geo.make(state = "CA"), endyear = year_sing, variable = sex.age.m, col.names = "pretty" )
+      ca.pop.f <- acs.fetch(geography = geo.make(state = "CA"), endyear = year_sing, variable = sex.age.f, col.names = "pretty" )
+      
+      ca.f.df <- data.frame(estimate(ca.pop.f))
+      ca.m.df <- data.frame(estimate(ca.pop.m))
+      names(ca.f.df) <- gsub("Sex.by.Age..Female..","",names(ca.f.df))
+      names(ca.m.df) <- gsub("Sex.by.Age..Male..","",names(ca.m.df))
+      ca.pop.df <- rbind(ca.m.df,ca.f.df)
+      ca.pop.df$Sex <- c("Male","Female")
+      ca.pop.df$Year <- year_sing
+      
+      ca.long <- melt(ca.pop.df, id.vars = c("Year","Sex"), variable.name = "Age", value.name = "Population")
+      ca.long <- ca.long %>%
+        mutate(Age = revalue(Age, c("15.to.17.years" = "15.to.19.years",
+                                    "18.and.19.years" = "15.to.19.years",
+                                    "20.years"="20.to.24.years",
+                                    "21.years"="20.to.24.years",
+                                    "22.to.24.years" ="20.to.24.years", 
+                                    "60.and.61.years"="60.to.64.years", 
+                                    "62.to.64.years"="60.to.64.years",
+                                    "65.and.66.years"="65.to.69.years",
+                                    "67.to.69.years"="65.to.69.years"))) %>%
+        group_by(Year,Sex, Age) %>%
+        summarise(Population =sum(Population))
+      
+      ca.long$Age <- gsub(".years","",ca.long$Age)
+      ca.long$Age <- gsub(".to.","-",ca.long$Age)
+      ca.long$Age[ca.long$Age=="Under.5"] <- "0-4"
+      ca.long$Age[ca.long$Age=="85.and.over"] <- "85+"
+      
+      return(ca.long)
+    }
+    
+  sacslist <- lapply(year, function(x) sacs_single(year_sing=x))
+  acs.df <- do.call(rbind.data.frame, sacslist)
+  return(acs.df)
 }
 
 
-#County level data
+#CA County level data
 cacs <- function(state,county, year){
   st <- gsub(" ","",toupper(state))
   cnty <- ifelse(county!=.,gsub(" ","",county), "*")
