e867d42beb0bb3e25f5e6f4f644265f659e54faa
Savet Hong <savet3@gmail.com>
2017-05-07 11:11:03 -0700

    Modified county function to generate multiple tables for multiple years.

--- acs_data.R
+++ acs_data.R
@@ -6,7 +6,7 @@
 ######################
 
 # CA State only
-acs <- function(year){
+sacs <- function(year){
     sacs_single <- function(year_sing){
       sex.age.m <- acs.lookup(table.number = "B01001", endyear = year_sing, keyword = "Male")[-1]
       sex.age.f <- acs.lookup(table.number = "B01001", endyear = year_sing, keyword = "Female")[-1]
@@ -49,44 +49,52 @@ acs <- function(year){
 }
 
 
-#CA County level data
-cacs <- function(state,county, year){
-  st <- gsub(" ","",toupper(state))
-  cnty <- ifelse(county!=.,gsub(" ","",county), "*")
-
-  sex.age.m <- acs.lookup(table.number = "B01001", endyear = year, keyword = "Male")[-1]
-  sex.age.f <- acs.lookup(table.number = "B01001", endyear = year, keyword = "Female")[-1]
-  ca.pop.m <- acs.fetch(geography = geo.make(state = "CA",county = cnty), endyear = year, variable = sex.age.m, col.names = "pretty" )
-  ca.pop.f <- acs.fetch(geography = geo.make(state = "CA",county = cnty), endyear = year, variable = sex.age.f, col.names = "pretty" )
+#CA ALL County level data
+cacs <- function(year){
+  cacs_single <- function(year_sing){
+    sex.age.m <- acs.lookup(table.number = "B01001", endyear = year_sing, keyword = "Male")[-1]
+    sex.age.f <- acs.lookup(table.number = "B01001", endyear = year_sing, keyword = "Female")[-1]
+    ca.pop.m <- acs.fetch(geography = geo.make(state = "CA", county = "*"), endyear = year_sing, variable = sex.age.m, col.names = "pretty" )
+    ca.pop.f <- acs.fetch(geography = geo.make(state = "CA", county = "*"), endyear = year_sing, variable = sex.age.f, col.names = "pretty" )
+    
+    ca.f.df <- data.frame(estimate(ca.pop.f))
+    ca.m.df <- data.frame(estimate(ca.pop.m))
+    names(ca.f.df) <- gsub("Sex.by.Age..Female..","",names(ca.f.df))
+    names(ca.m.df) <- gsub("Sex.by.Age..Male..","",names(ca.m.df))
+    ca.f.df$County <- gsub(" County, California","",row.names(ca.f.df))
+    ca.m.df$County <- gsub(" County, California","",row.names(ca.m.df))
+    ca.f.df$Sex <- "Female"
+    ca.m.df$Sex <- "Male"
+    ca.pop.df <- rbind(ca.m.df,ca.f.df)
+    ca.pop.df$Year <- year_sing
+    
+    ca.long <- melt(ca.pop.df, id.vars = c("Year","County","Sex"), variable.name = "Age", value.name = "Population")
+    ca.long <- ca.long %>%
+      mutate(Age = revalue(Age, c("15.to.17.years" = "15.to.19.years",
+                                  "18.and.19.years" = "15.to.19.years",
+                                  "20.years"="20.to.24.years",
+                                  "21.years"="20.to.24.years",
+                                  "22.to.24.years" ="20.to.24.years", 
+                                  "60.and.61.years"="60.to.64.years", 
+                                  "62.to.64.years"="60.to.64.years",
+                                  "65.and.66.years"="65.to.69.years",
+                                  "67.to.69.years"="65.to.69.years"))) %>%
+      group_by(Year,County,Sex, Age) %>%
+      summarise(Population =sum(Population))
+    
+    ca.long$Age <- gsub(".years","",ca.long$Age)
+    ca.long$Age <- gsub(".to.","-",ca.long$Age)
+    ca.long$Age[ca.long$Age=="Under.5"] <- "0-4"
+    ca.long$Age[ca.long$Age=="85.and.over"] <- "85+"
+    
+    return(ca.long)
+  }
   
-  ca.f.df <- data.frame(estimate(ca.pop.f))
-  ca.m.df <- data.frame(estimate(ca.pop.m))
-  names(ca.f.df) <- gsub("Sex.by.Age..Female..","",names(ca.f.df))
-  names(ca.m.df) <- gsub("Sex.by.Age..Male..","",names(ca.m.df))
-  ca.pop.df <- rbind(ca.m.df,ca.f.df)
-  ca.pop.df$Sex <- c("Male","Female")
-  
-  ca.long <- melt(ca.pop.df, id.vars = c("Year","county","Sex"), variable.name = "Age", value.name = "Population")
-  ca.long <- ca.long %>%
-    mutate(Age = revalue(Age, c("15.to.17.years" = "15.to.19.years",
-                                "18.and.19.years" = "15.to.19.years",
-                                "20.years"="20.to.24.years",
-                                "21.years"="20.to.24.years",
-                                "22.to.24.years" ="20.to.24.years", 
-                                "60.and.61.years"="60.to.64.years", 
-                                "62.to.64.years"="60.to.64.years",
-                                "65.and.66.years"="65.to.69.years",
-                                "67.to.69.years"="65.to.69.years"))) %>%
-    group_by(Year,county,Sex, Age) %>%
-    summarise(Population =sum(Population))
-  
-  ca.long$Age <- gsub(".years","",ca.long$Age)
-  ca.long$Age <- gsub(".to.","-",ca.long$Age)
-  ca.long$Age[ca.long$Age=="Under.5"] <- "0-4"
-  ca.long$Age[ca.long$Age=="85.and.over"] <- "85+"
-  
-  return(ca.long)
+  cacslist <- lapply(year, function(x) cacs_single(year_sing=x))
+  acs.df <- do.call(rbind.data.frame, cacslist)
+  return(acs.df)
 }
+
 
 
 # Add in proportion and Sex Ratio into Dataframe : State
